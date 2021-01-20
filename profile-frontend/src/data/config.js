927f3f6f93ee380056d57f230e95253d574c2914
rutuja-patil107 <68247202+rutuja-patil107@users.noreply.github.com>
2021-01-20 14:50:21 +0530

    First commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Rutuja Patil",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Rutuja Patil and I'm a Software Engineer!",
+  githubUrl: "https://github.com/rutuja-patil923",
+  linkedinUrl: "https://www.linkedin.com/in/rutuja-patil-511165171/",
+  cvLink: "https://docs.google.com/document/d/1TYfEF9xrGxzF0d25VIa-vWhaBvRSmcW9v-5-ZEQY5yU/edit?usp=sharing",
+};
